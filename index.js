4971946ca5a5462f0dec17c1aeed782089f96f3c
Shajal Ahamed <shajalahamed@Shajals-MacBook-Pro.local>
2018-12-26 23:26:07 +0600

    redis based search functionality

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+"use strict";
+
 const redis = require('redis');
 const redisClient = redis.createClient();
 
+/*
 redisClient.zadd('animal', 1 , 'cat');
 redisClient.zadd('animal', 2, 'cow');
 redisClient.zadd('animal', 3, 'catfish');
-redisClient.zadd('animal', 4, 'croccdile');
+redisClient.zadd('animal', 4, 'croccodile');
 redisClient.zincrby('animal', 5, 'cat');
 
 redisClient.zscore('animal', 'catfish', (err, score) =>{
@@ -15,4 +18,51 @@ redisClient.zscore('animal', 'catfish', (err, score) =>{
     }
 });
 
+redisClient.quit();
+*/
+
+/*
+class TypeAhead{
+    constructor(){
+        this.words = []
+    }
+
+    batchImport(words){
+
+    }
+}
+*/
+
+const batchImport = (words) => {
+    words.forEach(word => {
+        const prefixes = extractPrefixes(word);
+        index(prefixes, word);
+    });
+};
+const  extractPrefixes = (word) => {
+    const prefixes = [];
+    for(let index = 1; index <= word.length; index++){
+        prefixes.push(word.slice(0,index));
+    }
+    //console.log(prefixes);
+    return prefixes;
+}
+
+const index = (prefixes, word)=>{
+    prefixes.forEach(prefix => {
+        redisClient.zadd(prefix, 0, word);
+        //console.log(prefix + " 0 "+word);
+    });
+}
+
+const names = ['shajal','jayed', 'sajib', 'sazid', 'sourov'];
+
+batchImport(names);
+
+const search = (prefix) =>{
+    redisClient.zrange(prefix, 0, -1, (err, data)=>{
+        console.log(data);
+    });
+}
+search('sa')
 redisClient.quit();
